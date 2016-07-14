24dc8dc874e9f52657830b9cc8f51ed147899dc2
Brian Peiris <brianpeiris@gmail.com>
2016-07-14 16:51:58 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+
+'use strict'
+
+const express = require('express')
+const proxy = require('express-http-proxy')
+const bodyParser = require('body-parser')
+const _ = require('lodash')
+const config = require('./config')
+
+let bot = require('./bot')
+
+let app = express()
+
+if (config('PROXY_URI')) {
+  app.use(proxy(config('PROXY_URI'), {
+    forwardPath: (req, res) => { return require('url').parse(req.url).path }
+  }))
+}
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => { res.send('\n 👋 🌍 \n') })
+
+app.listen(config('PORT'), (err) => {
+  if (err) throw err
+
+  console.log(`\n🚀  Starbot LIVES on PORT ${config('PORT')} 🚀`)
+
+  if (config('SLACK_TOKEN')) {
+    console.log(`🤖  beep boop: @starbot is real-time\n`)
+    bot.listen({ token: config('SLACK_TOKEN') })
+  }
+})
